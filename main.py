236c6ca290b15c23581d90772cb2c9f70eb6473a
Henry Osei <34773810+hosei16@users.noreply.github.com>
2021-06-23 22:11:07 +0000

    Nathan

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ age = 32
 plays_basketball = True
 jersey_number = 13
 beard_length = 12.5
+
+
+
+
+
+print(type(name))
+print(type(age))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(beard_length))
